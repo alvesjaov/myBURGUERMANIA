3078bc944e2daaee1f6841d0c8201fbdbe93d95f
alvesjaov <victoralves169@outlook.com>
2024-11-19 20:27:03 -0300

    Adiciona ícones do Instagram e WhatsApp, atualiza o layout da página inicial e estiliza novos elementos de localização e contato

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { ButtonModule } from '../../components/button/button.module';
 import { NgOptimizedImage } from '@angular/common';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage, ButtonModule], // Use o módulo em vez do componente diretamente
+  imports: [HeaderComponent, NgOptimizedImage, ButtonModule, FooterComponent], // Use o módulo em vez do componente diretamente
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
