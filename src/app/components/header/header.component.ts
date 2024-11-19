e42eb65eccf12e2d220475b602a5e914c2d6cf58
alvesjaov <victoralves169@outlook.com>
2024-11-19 18:21:09 -0300

    Adiciona lógica para ocultar links de navegação no cabeçalho quando na página inicial

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,18 @@
-import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isHomePage: boolean;
 
+  constructor(private readonly router: Router) {
+    this.isHomePage = this.router.url === '/';
+  }
 }
