2b66697d294c276611bc5dcb13d084862cd48393
alvesjaov <victoralves169@outlook.com>
2024-11-20 11:13:35 -0300

    Atualiza o componente Menu com novas seções de cardápio, estilização aprimorada e funcionalidade para exibir o cardápio completo

--- src/app/views/menu/menu.component.ts
+++ src/app/views/menu/menu.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importando CommonModule
 import { HeaderComponent } from '../../components/header/header.component';
-import { ButtonModule } from '../../components/button/button.module';
+import { ButtonModule } from '../../components/button/button.module'; // Importando o módulo ButtonModule
 import { NgOptimizedImage } from '@angular/common';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage, ButtonModule, FooterComponent], // Use o módulo em vez do componente diretamente
+  imports: [CommonModule, HeaderComponent, NgOptimizedImage, ButtonModule, FooterComponent, CardComponent], // Adicionando CommonModule às importações
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+  showFullMenu: boolean = false;
 
+  toggleFullMenu() {
+    this.showFullMenu = !this.showFullMenu;
+  }
 }
