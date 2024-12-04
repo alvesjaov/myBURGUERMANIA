ee7af8ca8fd25db67d4b5b8eddb582f6bd52ed09
alvesjaov <victoralves169@outlook.com>
2024-12-04 00:04:24 -0300

    Adiciona descrição ao card e implementa busca de produtos da API no componente de menu

--- src/app/views/menu/menu.component.ts
+++ src/app/views/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common'; 
 import { HeaderComponent } from '../../components/header/header.component';
 import { ButtonModule } from '../../components/button/button.module'; 
@@ -13,12 +13,17 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
   showFullMenu: boolean = false;
   selectedProduct: any = null;
   quantity: number = 1;
+  products: any[] = [];
 
   constructor(private readonly http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchProducts();
+  }
 
   toggleFullMenu() {
     this.showFullMenu = !this.showFullMenu;
@@ -52,6 +57,24 @@ export class MenuComponent {
         this.selectedProduct = null; // Fechar o modal após adicionar o pedido
         this.quantity = 1; // Resetar a quantidade após adicionar o pedido
       });
+  }
+
+  fetchProducts() {
+    console.log('Iniciando a busca de produtos...');
+    this.http.get('https://myburguermania-api.onrender.com/api/product', {
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    })
+    .subscribe(
+      (response: any) => {
+        console.log('Resposta da API:', response);
+        this.products = response.produtos;
+      },
+      (error) => {
+        console.error('Erro ao buscar produtos:', error);
+      }
+    );
   }
 
   generateRandomId(): string {
